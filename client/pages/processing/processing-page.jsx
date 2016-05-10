cad2f25fd75b7194bba67b9ac7e3b6d997a78d08
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2016-05-09 22:19:14 -0700

    Pages cleanup

--- client/pages/processing/processing-page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class ProcessingPage extends React.Component {
-  render() {
-    return <article className="processing">
-      <h1>processing</h1>
-    </article>;
-  }
-}
--- client/pages/sending/sending-page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class SendingPage extends React.Component {
-  render() {
-    return <article className="sending">
-      <h1>sending</h1>
-    </article>;
-  }
-}

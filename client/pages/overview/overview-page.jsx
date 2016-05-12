2325daee624f707f47f5e0ad67f66dc6cae6ade1
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2016-05-12 11:12:07 -0700

    Fixed Abilash'es CSS conflicts

--- client/pages/overview/overview-page.jsx
+++ client/pages/overview/overview-page.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import PieChart from 'react-simple-pie-chart';
+import styles from './overview.css';
 
 export default class OverviewPage extends React.Component {
   render() {
     return <article className="overview">
-      <h1>overview</h1>
+      <p className = "heading">Group Overview</p>
+      <PieChart className="pie"
+      slices={[
+        {
+          color: '#6666ff',
+          value: 10
+        },
+        {
+          color: '#ff99ff',
+          value: 10
+        },
+        {
+          color: '#b3cce6',
+          value: 10
+        }
+      ]}
+    />
+      <div id="rectangle1"></div><div className = "text">Jens</div>
+      <div id="rectangle2"></div><div className = "text">Madhura</div>
+      <div id="rectangle3"></div><div className = "text">Abhilash</div>
+      <button className = "btn-back"></button>
+      <button className = "btn-confirm"></button>
     </article>;
   }
 }

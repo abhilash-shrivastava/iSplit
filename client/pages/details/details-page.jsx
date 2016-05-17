5219f1f86fb101f6ed2bafff6090508dd3d46d91
Ivan Gonzalez <Ivanglez43@gmail.com>
2016-05-16 17:25:25 -0700

    made adjustments to accomodate the list of items

--- client/pages/details/details-page.jsx
+++ client/pages/details/details-page.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './details.css';
+import {connect} from 'react-redux';
+import BillItem from '../../components/bill-item/bill-item.jsx';
 
 export default class DetailsPage extends React.Component {
   render() {
-    return <article className="details">
-      <div className="info-div">
+    return <article className="details" >
+      <div className="info-div" id="top">
         <p className="heading">YOU OWE</p>
         <img src="http://i.imgur.com/7JjkM0o.png"/>
         <p className="name">JOHN RAINIER</p>
       </div>
+      <div className="bill-list" id="bottom">
+        {Object.keys(this.props.bill || {}).map(key => {
+          return <BillItem key={key} item={this.props.bill[key]}></BillItem>;
+        })}
+      </div>
+
     </article>;
+
   }
 }

4ef43788bb1576e4685dc55c4c4f39bb4fe34ef4
Ivan Gonzalez <Ivanglez43@gmail.com>
2016-05-16 18:39:33 -0700

    page now renders list view

--- client/pages/details/details-page.jsx
+++ client/pages/details/details-page.jsx
@@ -3,7 +3,15 @@ import styles from './details.css';
 import {connect} from 'react-redux';
 import BillItem from '../../components/bill-item/bill-item.jsx';
 
+
 export default class DetailsPage extends React.Component {
+  componentWillMount() {
+    let pKeys = Object.keys(this.props.people);
+
+    for (let pKey of pKeys) {
+      let person = this.props.people[pKey];
+    }
+  }
   render() {
     return <article className="details" >
       <div className="info-div" id="top">
@@ -12,12 +20,15 @@ export default class DetailsPage extends React.Component {
         <p className="name">JOHN RAINIER</p>
       </div>
       <div className="bill-list" id="bottom">
-        {Object.keys(this.props.bill || {}).map(key => {
-          return <BillItem key={key} item={this.props.bill[key]}></BillItem>;
+        {Object.keys(this.props.people || {}).map(key => {
+          return <BillItem key={key} item={this.props.people[key]}></BillItem>;
         })}
       </div>
-
     </article>;
-
   }
 }
+const exports = connect(state => ({
+  people: state.root.people
+})
+)(DetailsPage);
+export default exports;

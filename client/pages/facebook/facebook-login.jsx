cddeeae99fa33c4dc5a82d2e7d79048095017e36
Abhilash Shrivastava <abhilash.shrivastava@hotmail.com>
2016-05-16 20:31:08 -0700

    get image from facebook

--- client/pages/facebook/facebook-login.jsx
+++ client/pages/facebook/facebook-login.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
+import styles from './facebook.css';
+import {connect} from 'react-redux';
 
-class MyComponent extends React.Component {
+class FacebookPage extends React.Component {
+  componentWillMount = () => {
+    this.setState({'imageUrl': null});
+  };
   responseFacebook = response => {
-    console.log(response);
+    //console.log(response);
+    FB.api(
+      "/" + response.id + "/picture",
+      function(response) {
+        if (response && !response.error) {
+          /* handle the result */
+          this.setState({
+            'imageUrl': response.data.url
+          });
+          //console.log(response.data.url);
+        }
+      }
+    );
   };
 
   render() {
-    return (
+    return <article className="facebook-btn">
       <FacebookLogin
         appId="1704240733167659"
         autoLoad={true}
         callback={this.responseFacebook} />
-    );
+      <img className="profile-picture" src={this.state.imageUrl}/>
+    </article>;
   }
 }
 
-export default MyComponent;
+export default FacebookPage;
+const exports = connect(
+  state => ({
+    image: state.root.image
+  })
+)(FacebookPage);
+export default exports;
